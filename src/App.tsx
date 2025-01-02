0e568fcdcc8b53c5d2d638a917a510d912560dc4
ThembaMtshelwane <thembamm3@gmail.com>
2025-01-03 00:02:13 +0200

    intro section minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ export default function App() {
   return (
     <section>
       <div className="h-screen relative sm:grid grid-cols-2 ">
-        <div className="p-2 flex flex-col md:px-10">
-          <h1 className="text-5xl md:text-6xl  lg:text-7xl mt-10 mb-5 font-semibold">
+        <div className="px-5 flex flex-col justify-center h-full max-w-[450px] mx-auto md:max-w-[680px]">
+          <h1 className="text-5xl md:text-6xl  lg:text-7xl mt-10 mb-2 font-semibold">
             The Digital Modelling of the Temple of Kulkukan
           </h1>
-          <p>
+          <p className="text-justify text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
             alias blanditiis nulla ducimus soluta possimus rerum impedit a
             nesciunt nisi eveniet rem deleniti dolores illum consequatur ab eos
