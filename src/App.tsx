5d5be929248b918af1210aadfbb5534afa5bdccd
ThembaMtshelwane <thembamm3@gmail.com>
2025-01-02 23:52:49 +0200

    Intro section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <section>
+      <div className="h-screen relative sm:grid grid-cols-2 ">
+        <div className="p-2 flex flex-col md:px-10">
+          <h1 className="text-5xl md:text-6xl  lg:text-7xl mt-10 mb-5 font-semibold">
+            The Digital Modelling of the Temple of Kulkukan
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+            alias blanditiis nulla ducimus soluta possimus rerum impedit a
+            nesciunt nisi eveniet rem deleniti dolores illum consequatur ab eos
+            velit debitis saepe placeat sapiente! Deleniti, officiis. Eum
+            veritatis, ex nihil facilis debitis iste rerum reprehenderit
+            officiis error molestiae officia omnis alias.
+          </p>
+        </div>
+        <div>
+          <img
+            src="intro.jpg"
+            alt="temple of kulkukan"
+            className="w-full h-full object-cover object-center opacity-45 absolute top-0 -z-10 sm:relative sm:opacity-100 sm:h-screen"
+          />
+        </div>
+      </div>
+      <div>Model demo</div>
+      <div>Results</div>
+    </section>
+  );
 }
