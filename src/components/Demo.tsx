2936c3342ca762e97ed9e9ce23902377ac39ef22
ThembaMtshelwane <thembamm3@gmail.com>
2025-01-11 21:45:41 +0200

    Adding input and output visuals

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -13,13 +13,13 @@ const Demo = () => {
           "https://tok-model-jd4k.vercel.app/systems/best_impulse_response"
         );
         // Set the data in state
-        setImpulseResponse(response.data);
+        setImpulseResponse(response.data.best_impulse_response);
       } catch (err) {
         console.error("Error fetching the impulse response:", err);
         setError("Unable to fetch data. Please try again later.");
       }
     };
-    fetchImpulseResponse(); 
+    fetchImpulseResponse();
   }, []);
 
   if (error) {
@@ -48,9 +48,13 @@ const Demo = () => {
       {impulseResponse}
 
       <div className="grid h-screen gap-4 place-items-center my-4 md:grid-cols-3 md:h-[50vh] max-w-[320px] md:max-w-[1024px] mx-auto ">
-        <div className="bg-red-600 rounded-full size-48 md:size-60 lg:size-80 animate-pulse-custom"></div>
+        <div className="size-48 md:size-60 lg:size-80 animate-pulse-custom">
+          <img src="hand-clap.png" className="scale-95" alt="" />
+        </div>
         <div className="bg-red-600 rounded-full size-48 md:size-60 lg:size-80"></div>
-        <div className="bg-red-600 rounded-full size-48 md:size-60 lg:size-80 animate-pulse-custom"></div>
+        <div className=" rounded-full size-48 md:size-60 lg:size-80 animate-pulse-custom flex">
+          <img src="q-bird.png" className="h-full w-full object-cover object-center" alt="" />
+        </div>
       </div>
 
       <p>
